c3beda8919783596d237d117d3847faf4249a136
minje0204 <minje@ajou.ac.kr>
2022-06-08 19:15:10 +0900

    initial commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd ../
+mkdir output
+cp -R ./minje-Algorithm/* ./output
+cp -R ./output ./minje-Algorithm/
---
+++ src/week01/Baekjoon_9466_텀프로젝트.java
@@ -0,0 +1,7 @@
+package week01;
+
+public class Baekjoon_9466_텀프로젝트 {
+    public static void main(String[] args) {
+        System.out.println("helloWorld!");
+    }
+}
