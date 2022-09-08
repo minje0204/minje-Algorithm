809cdb9735d422a062493472c1b1ee8eafd91a3e
Minje Cho <32082727+minje0204@users.noreply.github.com>
2022-09-08 22:57:02 +0900

    [Gold V] Title: 숫자고르기, Time: 228 ms, Memory: 18244 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -2,16 +2,20 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Stack;
 
 public class Main {
 
     static int N;
     static int[] adjList;
     static ArrayList<Integer> ansList = new ArrayList<>();
+    static Stack<Integer> stack = new Stack();
     static boolean isVisited[];
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         N = Integer.parseInt(br.readLine());
 
         adjList = new int[N + 1];
@@ -22,36 +26,40 @@ public class Main {
 
         for (int i = 1; i <= N; i++) {
             if (!isVisited[i]) {
-                ArrayList<Integer> visitedList = new ArrayList<>();
-                if (dfs(i, visitedList)) {
-                    ansList.addAll(visitedList);
-                }
+                stack.clear();
+                dfs(i);
             }
         }
 
-        System.out.println(ansList.size());
+        sb.append(ansList.size()).append("\n");
         ansList.sort(Comparator.comparingInt(a -> a));
         for (Integer a : ansList) {
-            System.out.println(a);
+            sb.append(a).append("\n");
         }
+
+        System.out.println(sb);
     }
 
-    public static boolean dfs(int start, ArrayList<Integer> visitedList) {
-        visitedList.add(start);
+    public static boolean dfs(int start) {
+        stack.push(start);
         isVisited[start] = true;
 
         int next = adjList[start];
-        if (!isVisited[next] && dfs(next, visitedList)) {
+        if (!isVisited[next] && dfs(next)) {
             return true;
         }
-        if (visitedList.contains(next)) {
-            for (int i = visitedList.indexOf(next) - 1; i >= 0; i--) {
-                visitedList.remove(i);
+        if (stack.contains(next)) {
+            while (!stack.isEmpty()) {
+                int top = stack.pop();
+                ansList.add(top);
+                if (top == next) {
+                    break;
+                }
             }
             return true;
         }
 
-        visitedList.remove(visitedList.size()-1);
+        stack.pop();
         return false;
     }
 }
