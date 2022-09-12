76e16bcb781d936cc94ee09bc5d099c20b0b9f7e
Minje Cho <32082727+minje0204@users.noreply.github.com>
2022-09-12 23:00:29 +0900

    [Gold V] Title: 숫자고르기, Time: 76 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Stack;
 
@@ -32,7 +33,7 @@ public class Main {
         }
 
         sb.append(ansList.size()).append("\n");
-        ansList.sort(Comparator.comparingInt(a -> a));
+        Collections.sort(ansList);
         for (Integer a : ansList) {
             sb.append(a).append("\n");
         }
